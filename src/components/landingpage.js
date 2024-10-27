002829704716cb43ffb468c1ad140648f99573db
Prakash Mariappan <86356130+prakashmariappan@users.noreply.github.com>
2024-10-26 22:41:26 -0500

    first-commit

---
+++ src/components/landingpage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Landingpage = () => {
+  return (
+    <>
+      <div class="landing-container">
+        <div class="animate-spooky"></div>
+        <div class="content">
+          <h1 class="logo-heading">Welcome to Spooky Spender</h1>
+          <p class="landing-description">
+            Haunt your finances no more. Track your spending and conjure a
+            budget that won’t send chills down your spine!
+          </p>
+          <button class="get-started">Get Started</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Landingpage;
---
+++ src/link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landingpage from './components/landingpage';
+
+const Link = () => {
+    return (
+        <div>
+            <Landingpage/>
+        </div>
+    );
+}
+
+export default Link;
